d655033d5619bc67b2c5020dba83b7f24af8dc7e
Aleksei Kraskov <kraskovalexey@yandex.ru>
2022-11-23 15:30:13 +0300

    Update ItemRequestDto.java

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
     private long id;
     private String description;
